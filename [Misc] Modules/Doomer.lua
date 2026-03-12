aaac69bc325e598cf6bb65ee2d86e873eb755a99
valttasar <TerexusCRAZEN@gmail.com>
2026-03-12 08:56:03 +0800

    Add files via upload

---
+++ [Misc] Modules/Doomer.lua
@@ -0,0 +1,59 @@
+-- UhhhhhhReanim/Modules/DoomerDance.lua
+
+local modules = {}
+
+table.insert(modules, function()
+	local m = {}
+
+	m.ModuleType  = "DANCE"
+	m.Name        = "Doomer"
+	m.Description = "GG desu GG desu"
+	m.Assets      = {"Doomer.anim@https://github.com/valttasar/Valttas-Port-Content/raw/refs/heads/main/Doomer/Doomer.anim", "Doomer2.anim@https://github.com/valttasar/Valttas-Port-Content/raw/refs/heads/main/Doomer/Doomer2.anim", "Doomer.mp3@https://github.com/valttasar/Valttas-Port-Content/raw/refs/heads/main/Doomer/Doomer.mp3"}
+
+	m.Alternative = false
+	m.Config = function(parent)
+		Util_CreateSwitch(parent, "Alt. Version", m.Alternative).Changed:Connect(function(val)
+			m.Alternative = val
+		end)
+	end
+
+	m.LoadConfig = function(save: any)
+		m.Alternative = not not save.Alternative
+	end
+
+	m.SaveConfig = function()
+		return {
+			Alternative = m.Alternative
+		}
+	end
+
+	local animator = nil
+	local start = 0
+
+	m.Init = function(figure: Model)
+		start = os.clock()
+		animator        = AnimLib.Animator.new()
+		animator.rig    = figure
+		animator.looped = true
+		animator.speed  = 1
+		if m.Alternative then
+			animator.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("Doomer2.anim"))
+			SetOverrideDanceMusic(AssetGetContentId("Doomer.mp3"), "Doomer", 1, NumberRange.new(0, 22.54))
+		else
+			animator.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("Doomer.anim"))
+			SetOverrideDanceMusic(AssetGetContentId("Doomer.mp3"), "Doomer", 1, NumberRange.new(0, 28.92))
+		end
+	end
+
+	m.Update = function(dt, figure)
+		animator:Step(os.clock() - start)
+	end
+
+	m.Destroy = function(figure)
+		animator = nil
+	end
+
+	return m
+end)
+
+return modules
